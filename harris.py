19a9c87e8f2bf07f24a1a2dfde2a5c043c1725e8
Lou <tuntun0847@gmail.com>
2015-04-16 01:05:23 +0800

    Finished Harris Corner Detection

---
+++ harris.py
@@ -0,0 +1,111 @@
+import numpy
+import cv2
+
+
+### read image file here
+def readFile():
+    img_bgr = cv2.imread('./sample/cow.jpg')
+    return img_bgr
+
+### draw dots on img
+### default dot color is red
+### have to make copy to prevent drawing on original image
+def drawDots(img, dots):
+    tmp = img.copy()
+    for pt in range(len(dots)):
+        point = (dots[pt][0], dots[pt][1])
+        cv2.circle(tmp, point, 1, (0, 0, 255), -1)
+
+    return tmp
+
+
+### Harris Corner Detection
+def harris(img_bgr):
+
+    ### parameters
+
+    sigma   = 0.05       ### sigma for calculating pixel sum
+    k       = 0.04      ### empirical constant 0.04-0.06
+    R_min   = 60000       ### threshold for harris corner detector
+
+    ### need gray scale only
+    img_y = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
+
+
+    ### Calculate x and y derivatives of img
+    ### cv2.Sobel already add Gaussian smoothing
+    ### to calculate derivatives without smoothing, use cv2.Sobel(src, depth, x_ord, y_ord, dst, 1) instead
+    ddepth = cv2.CV_32F
+    #ddepth = -1
+    Ix = cv2.Sobel(img_y, ddepth, 1, 0, None, 1)
+    Iy = cv2.Sobel(img_y, ddepth, 0, 1, None, 1)
+
+    ### Calculate Ixx, Iyy, Ixy
+    Ixx = Ix * Ix
+    Iyy = Iy * Iy
+    Ixy = Ix * Iy
+
+    ### Free memory
+    del Ix
+    del Iy
+
+    ### Calculate pixel sum of Ixx, Iyy, Ixy with noise filtering
+    Sxx = cv2.GaussianBlur(Ixx, (3, 3), sigma)
+    Syy = cv2.GaussianBlur(Iyy, (3, 3), sigma)
+    Sxy = cv2.GaussianBlur(Ixy, (3, 3), sigma)
+
+    ### Free memory again
+    del Ixx
+    del Iyy
+    del Ixy
+
+    ### Calculate R
+    ### Calculation of M is skipped since we only need det(M) and trace(M)
+    det     = (Sxx * Syy) - Sxy**2
+    trace   = Sxx + Syy 
+    R = det - k*(trace**2)
+    R = numpy.abs(R)
+
+
+    ### delete point by finding local maximum
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(12,12))
+    R_dilate = cv2.dilate(R, kernel) 
+    R[R!=R_dilate] = 0          ### remove point that are not local maximum
+    
+    ### Free memory
+    del det, R_dilate
+    del trace
+
+
+    ### Find corners and draw dots on original map at harris corner
+    corners = numpy.transpose((R>R_min).nonzero())
+    corners = numpy.fliplr(corners)         ### (row, column) = (y, x) so need to switch 
+    img_bgr = drawDots(img_bgr, corners)
+
+
+    ### Generate harris corner image by opencv function
+    ### This is for comparison
+    ### GoodFeaturesToTrack
+    corners_good = cv2.goodFeaturesToTrack(img_y, 500, 0.0005, 10, None,  None, 3, True, k)
+    corners_good = numpy.reshape(corners_good, (len(corners_good), 2) )
+    img_good = drawDots(img_bgr, corners_good)
+    cv2.imwrite('cv_good.jpg', img_good)
+
+    return img_bgr
+
+
+
+
+     
+
+    
+
+
+### main function
+if __name__ == "__main__":
+    img_bgr = readFile()
+    img_result = harris(img_bgr)
+    cv2.imwrite('corner_harris.jpg', img_result)
+
+
+
