41d4cb390908539f91ba43da02d65560be93b6b5
obrightness <tuntun0847@gmail.com>
2015-04-18 00:30:01 +0800

    Fixed Projection
            Previous projection take (x, y) and (x', y') wrong.....(opposite)
            moved projection center to (width/2, height/2) instead of (0, 0)
            black border are add to not truncate anythin

--- projection.py
+++ projection.py
@@ -7,29 +7,32 @@ from harris import readFile
 ### get pixel value at point(row, col) on img
 ### no interpolation is used for now
 ### f = focal length
+### sample image set 1: f = 704 is ok
 def getPixelValue(row, col, img, f):
     
     ### parameter
     s = f
-    
+    width = len(img[0])
+    height = len(img)
 
+    ### put coordinate center to (width/2, height/2)
+    col = col - width/2 
+    row = row - height/2 
+    row = row - 10
 
     ### coordiante transformation 
-    theta = math.atan(col/f) 
-    h = row / math.sqrt( col*col + f*f )
+    ### horizontal center should be at center of img
+    new_col = f*math.tan(col/s)
+    new_row = (row/s) * math.sqrt( new_col*new_col + f*f)
     
     ### no interpolation for now
     ### Use interpolation if result is not good
-    new_row = math.floor(s*h)
-    new_col = math.floor(s*theta)
+    new_col = math.floor(new_col) + width/2
+    new_row = math.floor(new_row) + height/2
 
     ### check for boarders
-    if new_row >= len(img):
+    if (new_row >= height) or (new_row < 0) or (new_col >= width) or (new_col < 0):
         return numpy.zeros(img[0][0].shape) 
-
-    if new_col >= len(img[0]):
-        return numpy.zeros(img[0][0].shape) 
-
 
     return img[new_row][new_col]
 
@@ -48,7 +51,7 @@ def cyCorrect(img, f):
     depth = len(img[0][0])
 
     ### get black boarder
-    row_max += 20
+    row_max += 40
     col_max += 20
     ### do the job
     result = numpy.zeros((row_max, col_max, depth))
@@ -65,7 +68,7 @@ def cyCorrect(img, f):
 ### main function
 if __name__ == "__main__":
     img_bgr = readFile('./sample/parrington/prtn13.jpg')
-    img_result = cyCorrect(img_bgr, 904.289)
+    img_result = cyCorrect(img_bgr, 704.289)
     cv2.imwrite('img_wrapped.jpg', img_result)
     
 
