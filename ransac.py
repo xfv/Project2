0fa89bc7f97718ed060821c7fdedf8ec0ca65124
obrightness <tuntun0847@gmail.com>
2015-04-19 00:45:44 +0800

    RANSAC ver 1
            this code is not complete!
            solve_M is not defined yet

---
+++ ransac.py
@@ -0,0 +1,55 @@
+import numpy
+import matching
+import cv2
+import random
+
+
+
+
+### Run RANSAC 
+### point 1 and point 2 shoud be paired 
+def ransac(points_1, points_2):
+
+    ### parameters
+    k = 4       ### only need four points to solve 2*2 matrix M
+    n = 1000    ### iterations
+    th = 1      ### threshold
+
+    ### dimension
+    len_1 = len(points_1)
+    len_2 = len(points_2)
+
+
+
+    ### main loop
+    random.seed()
+    inlier = numpy.array([])    ### this is the index of inliers
+    selected_1 = numpy.zeros((k, 2))
+    selected_2 = numpy.zeros((k, 2))
+
+    for itr in range(n):
+
+        ### pick k random feature points
+        for i in range(k):
+            selected_1[i] = points_1[random.randrange(len_1)]
+            selected_2[i] = points_2[random.randrange(len_2)]
+        
+        ### calculate Homography for these four points
+        homo = solve_M(selected_1, selected_2)
+
+        ### calculate distance and threshold
+        point_solved = numpy.inner(points_2, homo)
+        distance = numpy.sqrt( numpy.sum( (point_solved-points_1)**2, 1 ) )
+        tmp_inlier = numpy.transpose( (distance<th).nonzero() )
+        if len(tmp_inlier) > inlier:
+            inlier = tmp_inlier
+
+
+
+    return points_1[inlier], points_2[inlier]
+        
+
+
+
+
+
