0f3063f16c9b79c6c867c32c181ab7fbdacb7330
Anne <anne@tagtoo.org>
2015-04-20 15:48:31 +0800

    merge 2 version

--- matching.py
+++ matching.py
@@ -3,14 +3,14 @@ import cv2
 from scipy import ndimage
 
 def get_neighbors(x, y, rows, cols):
-    return np.array([(y-1, x-1), (y-1, x), (y-1, x+1), (y, x-1), (y, x), (y, x+1), (y+1, x-1), (y+1, x), (y+1, x+1)])
+    return np.array([(x-1, y-1), (x, y-1), (x+1, y-1), (x-1, y), (x, y), (x+1, y), (x-1, y+1), (x, y+1), (x+1, y+1)])
 
 def descriptor(img, xy):
     features = np.zeros((len(xy), 9))
     for i in range(len(xy)):
         neighbors = get_neighbors(xy[i, 0], xy[i, 1])
         for j in range(9):
-            features[i, j] = img[neighbors[j, 0], neighbors[j, 1]]
+            features[i, j] = img[neighbors[j, 1], neighbors[j, 0]]
     return features
 
 def find_pair(xy_1, features_1, xy_2, features_2):
@@ -29,8 +29,8 @@ def find_pair(xy_1, features_1, xy_2, features_2):
                 continue
         arg[min_arg] = f
         value[min_arg] = min_value
-    pair_1 = np.zeros((len(arg),2))
-    pair_2 = np.zeros((len(arg),2))
+    pair_1 = np.zeros((len(arg),2), dtype='uint8')
+    pair_2 = np.zeros((len(arg),2), dtype='uint8')
     pair_idx = 0
     for p in range(len(pair)):
         if not arg[pair[p]] == p:
@@ -45,16 +45,16 @@ def solve_M(pair_1, pair_2):
     A = np.zeros((len(pair_2)*2, 4))
     B = np.zeros((len(pair_1)*2, 1))
     for i in range(len(pair_1)):
-        B[2*i] = pair_1[i, 1]
-        B[2*i+1] = pair_1[i, 0]
-        A[2*i, 0] = pair_2[i, 1]
-        A[2*i, 1] = pair_2[i, 0]
-        A[2*i+1, 2] = pair_2[i, 1]
-        A[2*i+1, 3] = pair_2[i, 0]
+        B[2*i] = pair_1[i, 0]
+        B[2*i+1] = pair_1[i, 1]
+        A[2*i, 0] = pair_2[i, 0]
+        A[2*i, 1] = pair_2[i, 1]
+        A[2*i+1, 2] = pair_2[i, 0]
+        A[2*i+1, 3] = pair_2[i, 1]
 
     x = np.zeros((4,1))
     err, x = cv2.solve(A, B, x, cv2.DECOMP_SVD)
-    return x
+    return np.reshape(x, (2,2))
 
 def main():
     xy_1 = np.random.randint(10, size = (10,2))
