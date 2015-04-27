5d7811879ee8f686acbc746ee74335940aaef8e4
Anne <anne@tagtoo.org>
2015-04-27 22:22:41 +0800

    blur version

--- assemble.py
+++ assemble.py
@@ -8,15 +8,17 @@ def assemble(img_bgr, M):
     x_max = cols
     y_min = 0
     y_max = rows
-    for m in range(len(M)):
-        M[m] = np.concatenate((M[m], np.array([0,0,1]).reshape(1,3)), 0) # Extend M to 3x3 matrix
+#for m in range(len(M)):
+#        M[m] = np.concatenate((M[m], np.array([0,0,1]).reshape(1,3)), 0) # Extend M to 3x3 matrix
     position = []
-    for i in range(len(img_bgr)):
+    for i in range(1, len(img_bgr)):
         # Define d_size for output img
         d_size_x = np.array([[0, cols], [0, cols]])
         d_size_y = np.array([[0, 0], [rows, rows]])
         d_size_x = d_size_x*M[i-1][0,0] + d_size_y*M[i-1][0,1] + 1*M[i-1][0,2]
         d_size_y = d_size_x*M[i-1][1,0] + d_size_y*M[i-1][1,1] + 1*M[i-1][1,2]
+        d_size_x = d_size_x.astype(int)
+        d_size_y = d_size_y.astype(int)
         if max(d_size_x[0,1], d_size_x[1,1]) > x_max:
             x_max = max(d_size_x[0,1], d_size_x[1,1])
         if min(d_size_y[0,0], d_size_y[0,1]) < y_min:
@@ -44,8 +46,8 @@ def assemble(img_bgr, M):
         d_size_y = d_size_y.astype(int)
         print d_size_x
         print d_size_y
-        y_begin = min(d_size_y[0,0], d_size_y[0,1]) - y_min
-        y_end = max(d_size_y[1,0], d_size_y[1,1]) - y_min
+        y_begin = int(min(d_size_y[0,0], d_size_y[0,1]) - y_min)
+        y_end = int(max(d_size_y[1,0], d_size_y[1,1]) - y_min)
         x_begin = min(d_size_x[0,0], d_size_x[1,0])
         x_end = max(d_size_x[0,1], d_size_x[1,1])
         position.append([y_begin, y_end, x_begin, x_end])
@@ -53,14 +55,10 @@ def assemble(img_bgr, M):
         M[i-1][0,2] -= x_begin
         M[i-1][1,2] -= y_begin
         d_size = (x_end-x_begin+1, y_end-y_begin+1)
-#       x = cv2.warpPerspective(x.astype(float), M[i-1], d_size)
-#       y = cv2.warpPerspective(y.astype(float), M[i-1], d_size)
         warp_img_bgr = np.zeros((y_end-y_begin+1, x_end-x_begin+1, 3))
         warp_img_bgr[:,:,0] = cv2.warpPerspective(img_bgr[i][:,:,0], M[i-1], d_size)
         warp_img_bgr[:,:,1] = cv2.warpPerspective(img_bgr[i][:,:,1], M[i-1], d_size)
         warp_img_bgr[:,:,2] = cv2.warpPerspective(img_bgr[i][:,:,2], M[i-1], d_size)
-#      x = x.astype(int)
-#       y = y.astype(int)
 
         if position[i][0] > position[i-1][0]:
             wy_begin = position[i][0]
@@ -79,7 +77,7 @@ def assemble(img_bgr, M):
         mask_element_1 = np.mgrid[1:0:complex(nx),0:3]
         mask_element_2 = np.mgrid[0:1:complex(nx),0:3]
         w1[wy_begin:wy_end+1, wx_begin:wx_end+1, :] = np.tile(mask_element_1[0],(ny,1,1))
-        w2[(wy_begin-y_begin-y_min):(wy_end-y_begin-y_min+1), (wx_begin-x_begin):(wx_end-x_begin+1), :] = np.tile(mask_element_2[0],(ny,1,1)) 
+        w2[(wy_begin-y_begin):(wy_end-y_begin+1), (wx_begin-x_begin):(wx_end-x_begin+1), :] = np.tile(mask_element_2[0],(ny,1,1)) 
         panorama *= w1
         panorama[y_begin:y_end+1, x_begin:x_end+1, :] += warp_img_bgr*w2
     cv2.imwrite('panorama.jpg', panorama) 
--- main.py
+++ main.py
@@ -8,65 +8,62 @@ import assemble
 
 ### main function for panorama
 print 'reading files..'
-img_1 = harris.readFile('./parrington/prtn13.jpg')
-img_2 = harris.readFile('./parrington/prtn12.jpg')
+img = []
+M = []
+data_set = 3
+for i in range(data_set,-1,-1):
+    print 'loading', i
+    img.append(harris.readFile('./parrington/prtn' + str(i).zfill(2) + '.jpg'))
 
+for i in range(data_set):
+    print i
+    ### correction
+    print 'cyCorrect'
+    img_1 = img[i]
+    img_2 = img[i+1]
+    img_1_cy = projection.cyCorrect(img_1, 704.289)
+    img_2_cy = projection.cyCorrect(img_2, 704.0)
+    
+    ### get gray scale
+    img_1_gray = cv2.cvtColor(img_1_cy, cv2.COLOR_BGR2GRAY)
+    img_2_gray = cv2.cvtColor(img_2_cy, cv2.COLOR_BGR2GRAY)
+    
+    ### harris
+    ### points are(x, y) not (row, col)
+    print 'harris'
+    points_1 = harris.harris(img_1_gray)
+    points_2 = harris.harris(img_2_gray)
 
-### correction
-print 'cyCorrect'
-img_1_cy = projection.cyCorrect(img_1, 704.289)
-img_2_cy = projection.cyCorrect(img_2, 704.0)
-cv2.imwrite('img_1_cy.jpg', img_1_cy)
-cv2.imwrite('img_2_cy.jpg', img_2_cy)
-
-### get gray scale
-img_1_gray = cv2.cvtColor(img_1_cy, cv2.COLOR_BGR2GRAY)
-img_2_gray = cv2.cvtColor(img_2_cy, cv2.COLOR_BGR2GRAY)
-
-### harris
-### points are(x, y) not (row, col)
-print 'harris'
-points_1 = harris.harris(img_1_gray)
-points_2 = harris.harris(img_2_gray)
-
-### draw dots
-img_1_harris = harris.drawDots(img_1_cy, points_1)
-img_2_harris = harris.drawDots(img_2_cy, points_2)
-cv2.imwrite('img_1_harris.jpg', img_1_harris)
-cv2.imwrite('img_2_harris.jpg', img_2_harris)
-
-### get feature
-print 'getting features...'
-feature_1 = matching.descriptor(img_1_gray, points_1)
-feature_2 = matching.descriptor(img_2_gray, points_2)
-
-
-print 'matching...'
-pairs = matching.find_pair_2(points_1, feature_1, points_2, feature_2)
-print 'Got ', len(pairs[0]), ' pairs'
-start = 0 
-end = 250
-#print pairs[0][start:end], pairs[1][start:end]
-
-img_1 = harris.drawDots(img_1_cy, pairs[0][start:end])
-img_2 = harris.drawDots(img_2_cy, pairs[1][start:end])
-
-cv2.imwrite('img_1_pair.jpg', img_1)
-cv2.imwrite('img_2_pair.jpg', img_2)
-img_match = matching.drawMatchLine(img_1, img_2, pairs[0][start:end], pairs[1][start:end])
-cv2.imwrite('img_match_line.jpg', img_match)
-
-### run RANSAC
-### pairs = [ points_1, points_2 ]
-print 'RANSAC...'
-pairs = ransac.ransac(pairs[0], pairs[1])
-M = matching.solve_M(pairs[0], pairs[1])
-print 'Got ', len(pairs[0]), ' pairs'
-img_1 = harris.drawDots(img_1_cy, pairs[0][start:end])
-img_2 = harris.drawDots(img_2_cy, pairs[1][start:end])
-img_match = matching.drawMatchLine(img_1, img_2, pairs[0], pairs[1])
-cv2.imwrite('img_match_line_2.jpg', img_match)
+    ### draw dots
+    img_1_harris = harris.drawDots(img_1_cy, points_1)
+    img_2_harris = harris.drawDots(img_2_cy, points_2)
+    
+    ### get feature
+    print 'getting features...'
+    feature_1 = matching.descriptor(img_1_gray, points_1)
+    feature_2 = matching.descriptor(img_2_gray, points_2)
+    
+    print 'matching...'
+    pairs = matching.find_pair(points_1, feature_1, points_2, feature_2)
+    print 'Got ', len(pairs[0]), ' pairs'
+#   start = 0 
+#   end = 250
+#   print pairs[0][start:end], pairs[1][start:end]
+    
+    ### run RANSAC
+    ### pairs = [ points_1, points_2 ]
+    print 'RANSAC...'
+    pairs = ransac.ransac(pairs[0], pairs[1])
+    tmp_M = matching.solve_M(pairs[0], pairs[1])
+    tmp_M = numpy.concatenate((tmp_M, numpy.array([0,0,1]).reshape(1,3)), 0) # Extend M to 3x3 matrix
+    if i==0:
+        M.append(tmp_M)
+        print M[i]
+    else:
+        M.append(numpy.matrix(tmp_M)*numpy.matrix(M[i-1]))
+        print M[i]
+    print 'Got ', len(pairs[0]), ' pairs'
 
 ### run assemble
-#assemble.assemble([img_1_cy, img_2_cy], [M])
+assemble.assemble(img, M)
 
